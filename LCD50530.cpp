a92b0c2ef39ee001da8c2f9cef278971a62784c1
Jeroen Van Beirendonck <jeroen.vanbeirendonck@gmail.com>
2012-03-31 16:02:46 +0200

    Working code

--- LCD50530.cpp
+++ LCD50530.cpp
@@ -4,99 +4,65 @@
 #include <string.h>
 #include <inttypes.h>
 #include "Arduino.h"
-
-// When the display powers up, it is configured as follows:
-//
-// 1. Display clear
-// 2. Function set: 
-//    DL = 1; 8-bit interface data 
-//    N = 0; 1-line display 
-//    F = 0; 5x8 dot character font 
-// 3. Display on/off control: 
-//    D = 0; Display off 
-//    C = 0; Cursor off 
-//    B = 0; Blinking off 
-// 4. Entry mode set: 
-//    I/D = 1; Increment by 1 
-//    S = 0; No shift 
-//
-// Note, however, that resetting the Arduino doesn't reset the LCD, so we
-// can't assume that its in that state when a sketch starts (and the
-// LCD50530 constructor is called).
-
-LCD50530::LCD50530(uint8_t ioc1, uint8_t ioc2, uint8_t rw, uint8_t ex, uint8_t d0, uint8_t d1, uint8_t d2, uint8_t d3, uint8_t d4, uint8_t d5, uint8_t d6, uint8_t d7)
-{
-	init(0, ioc1, ioc2, rw, ex, d0, d1, d2, d3, d4, d5, d6, d7);
-}
-		
+	
 LCD50530::LCD50530(uint8_t ioc1, uint8_t ioc2, uint8_t rw, uint8_t ex, uint8_t d4, uint8_t d5, uint8_t d6, uint8_t d7)
 {
-	init(1, ioc1, ioc2, rw, ex, 0, 0, 0, 0, d4, d5, d6, d7);
+	init(ioc1, ioc2, rw, ex, d4, d5, d6, d7);
 }
 
-void LCD50530::init(uint8_t fourbitmode, uint8_t ioc1, uint8_t ioc2, uint8_t rw, uint8_t ex, uint8_t d0, uint8_t d1, uint8_t d2, uint8_t d3, uint8_t d4, uint8_t d5, uint8_t d6, uint8_t d7)
+void LCD50530::init(uint8_t ioc1, uint8_t ioc2, uint8_t rw, uint8_t ex, uint8_t d4, uint8_t d5, uint8_t d6, uint8_t d7)
 {
   _ioc1_pin = ioc1;
   _ioc2_pin = ioc2;
   _rw_pin = rw;
   _ex_pin = ex;
   
-  _data_pins[0] = d0;
-  _data_pins[1] = d1;
-  _data_pins[2] = d2;
-  _data_pins[3] = d3; 
-  _data_pins[4] = d4;
-  _data_pins[5] = d5;
-  _data_pins[6] = d6;
-  _data_pins[7] = d7; 
+  _data_pins[0] = d4;
+  _data_pins[1] = d5;
+  _data_pins[2] = d6;
+  _data_pins[3] = d7; 
 
-  DDRD = B11111111; // Default: Set all ports to output (Atmel shortcut)
-  
-  if (fourbitmode)
-    _displayfunction = LCD_4BITMODE | LCD_1LINE | LCD_5x8DOTS;
-  else 
-    _displayfunction = LCD_8BITMODE | LCD_1LINE | LCD_5x8DOTS;
-  
-  begin(16, 1);  
+  DDRD = B11111111; // Default: Set all ports to output (Atmel shortcut)    
 }
 
-void LCD50530::begin(uint8_t cols, uint8_t lines, uint8_t dotsize) {
-  /*
-  if (lines > 1) {
-    _displayfunction |= LCD_2LINE;
-  }
-  _numlines = lines;
-  _currline = 0;
+void LCD50530::begin(uint8_t cols, uint8_t lines, uint8_t dotsize) {  
+  int totalChar = cols*lines;
 
-  // for some 1 line displays you can select a 10 pixel high font
-  if ((dotsize != 0) && (lines == 1)) {
-    _displayfunction |= LCD_5x12DOTS;
-  }
+  uint8_t displaySize = 0x00;
   
-  //put the LCD into 4 bit or 8 bit mode
-  if (! (_displayfunction & LCD_8BITMODE)) {
-	// TODO: DO SOMETHING 
-  } else {
-
+  if(lines <= 1)
+	displaySize |= LCD_1LINE;
+  else if (lines == 2)
+	displaySize |= LCD_2LINE;
+  else 
+	displaySize |= LCD_4LINE;
+  
+  if(totalChar <= 160) {
+	displaySize |= LCD_160CHAR;
+	_cols = 160/lines;
   }
+  else if(totalChar <= 192) {
+	displaySize |= LCD_192CHAR;	
+	_cols = 192/lines;	
+  }
+  else if(totalChar <= 224) {
+	displaySize |= LCD_224CHAR; 
+	_cols = 224/lines;	
+  }
+  else{ // 256 characters - 
+	displaySize |= LCD_256CHAR;
+	_cols = 256/lines;
+  }	
+  
+  _functionmode = LCD_4BITMODE | LCD_5x8DOTS | displaySize;
+  command(LCD_FUNCTIONMODESET | _functionmode); 
+  
+  _displaymode = LCD_DISPLAYON;
+  command(LCD_DISPLAYMODESET | _displaymode);
 
-  // finally, set # lines, font size, etc.
-  command(LCD_FUNCTIONMODESET | _displayfunction);  
-
-  // turn the display on with no cursor or blinking default
-  _displaycontrol = LCD_DISPLAYON | LCD_CURSOROFF | LCD_CURSORBLINKOFF;  
-  display();
-
-  // clear it off
-  clear();
-
-  // Initialize to default text direction (for romance languages)
-  _displaymode = LCD_CURSORMOVEWRITE | LCD_CURSORMOVERIGHT;
-  // set the entry mode
-  command(LCD_ENTRYMODESET | _displaymode);
-  */
-
-  command(LCD_FUNCTIONMODESET | LCD_4BITMODE | LCD_5x8DOTS | LCD_4x40);
+  _entrymode = LCD_CURSORMOVEWRITE | LCD_CURSORMOVERIGHT;
+  command(LCD_ENTRYMODESET | _entrymode); 
+  
   command(LCD_CLEARDISPLAY);
 }
 
@@ -113,96 +79,126 @@ void LCD50530::home()
 
 void LCD50530::setCursor(uint8_t col, uint8_t row)
 {
-  uint8_t instruction = row*40+col;
-  send(instruction, 0x03);
+  uint8_t instruction = row*_cols+col;
+  send(instruction, 1<<_ioc1_pin | 1<<_ioc2_pin);
 }
 
 // Turn the display on/off (quickly)
 void LCD50530::noDisplay() {
-  _displaycontrol &= ~LCD_DISPLAYON;
-  command(LCD_DISPLAYMODESET | _displaycontrol);
+  _displaymode &= ~LCD_DISPLAYON;
+  command(LCD_DISPLAYMODESET | _displaymode);
 }
 void LCD50530::display() {
-  _displaycontrol |= LCD_DISPLAYON;
-  command(LCD_DISPLAYMODESET | _displaycontrol);
+  _displaymode |= LCD_DISPLAYON;
+  command(LCD_DISPLAYMODESET | _displaymode);
 }
 
 // Turns the underline cursor on/off
 void LCD50530::noCursor() {
-  _displaycontrol &= ~LCD_CURSORON;
-  command(LCD_DISPLAYMODESET | _displaycontrol);
+  _displaymode &= ~LCD_CURSORON;
+  command(LCD_DISPLAYMODESET | _displaymode);
 }
 void LCD50530::cursor() {
-  _displaycontrol |= LCD_CURSORON;
-  command(LCD_DISPLAYMODESET | _displaycontrol);
+  _displaymode |= LCD_CURSORON;
+  command(LCD_DISPLAYMODESET | _displaymode);
+}
+
+// Turns the underline cursor on/off
+void LCD50530::noUnderline() {
+  _displaymode &= ~LCD_UNDERLINEON;
+  command(LCD_DISPLAYMODESET | _displaymode);
+}
+void LCD50530::underline() {
+  _displaymode |= LCD_UNDERLINEON;
+  command(LCD_DISPLAYMODESET | _displaymode);
+}
+// Turn on and off the blinking cursor
+void LCD50530::noCursorBlink() {
+  _displaymode &= ~LCD_CURSORBLINKON;
+  command(LCD_DISPLAYMODESET | _displaymode);
+}
+void LCD50530::cursorBlink() {
+  _displaymode |= LCD_CURSORBLINKON;
+  command(LCD_DISPLAYMODESET | _displaymode);
 }
 
 // Turn on and off the blinking cursor
-void LCD50530::noBlink() {
-  _displaycontrol &= ~LCD_CURSORBLINKON;
-  command(LCD_DISPLAYMODESET | _displaycontrol);
+void LCD50530::noCharBlink() {
+  _displaymode &= ~LCD_CHRBLINKON;
+  command(LCD_DISPLAYMODESET | _displaymode);
 }
-void LCD50530::blink() {
-  _displaycontrol |= LCD_CURSORBLINKON;
-  command(LCD_DISPLAYMODESET | _displaycontrol);
+void LCD50530::charBlink() {
+  _displaymode |= LCD_CHRBLINKON;
+  command(LCD_DISPLAYMODESET | _displaymode);
 }
 
-// These commands scroll the display without changing the RAM
-void LCD50530::scrollDisplayLeft(void) {
-  command(LCD_CURSORSHIFT | LCD_DISPLAYMOVE | LCD_DISPLAYMOVELEFT);
+// These commands moves the cursor one space in the directopn without changing the RAM
+void LCD50530::shiftCursor(uint8_t direction) {    
+  command(LCD_SHIFTCOMMAND | LCD_CURSORSHIFT | (direction<<2));
 }
-void LCD50530::scrollDisplayRight(void) {
-  command(LCD_CURSORSHIFT | LCD_DISPLAYMOVE | LCD_DISPLAYMOVERIGHT);
+
+// These commands moves the display one space in the directopn without changing the RAM
+void LCD50530::shiftDisplay(uint8_t direction) {
+  command(LCD_SHIFTCOMMAND | LCD_DISPLAYSHIFT | direction);
+}
+// This is for text that flows Left to Right
+void LCD50530::moveCursorAfterInstruction(uint8_t direction, uint8_t instruction) {
+  _entrymode &= B01000111;
+  _entrymode |= ((instruction | direction) <<3);
+  command(LCD_ENTRYMODESET | _entrymode);
+}
+
+void LCD50530::moveDisplayAfterInstruction(uint8_t direction, uint8_t instruction) {
+  _entrymode &= B01111000;
+  _entrymode |= instruction | direction;
+  command(LCD_ENTRYMODESET | _entrymode);
+}
+
+void LCD50530::holdCursor(void) {
+	command(LCD_ENTRYMODESET & B01000111);
+}
+
+void LCD50530::holdDisplay(void) {
+	command(LCD_ENTRYMODESET & B01111000);
 }
 
 // This is for text that flows Left to Right
 void LCD50530::leftToRight(void) {
-  _displaymode |= LCD_DISPLAYMOVEWRITE | LCD_DISPLAYMOVELEFT;
-  command(LCD_ENTRYMODESET | _displaymode);
+  moveCursorAfterInstruction(LCD_WRITE, LCD_RIGHT);
 }
 
 // This is for text that flows Right to Left
 void LCD50530::rightToLeft(void) {
-  _displaymode &= ~LCD_DISPLAYMOVELEFT;
-  command(LCD_ENTRYMODESET | _displaymode);
+	moveCursorAfterInstruction(LCD_WRITE, LCD_LEFT);
 }
 
 // This will 'right justify' text from the cursor
 void LCD50530::autoscroll(void) {
-  _displaymode |= LCD_CURSORMOVEWRITE | LCD_CURSORMOVELEFT;
-  command(LCD_ENTRYMODESET | _displaymode);
+  moveDisplayAfterInstruction(LCD_WRITE, LCD_RIGHT);
 }
 
 // This will 'left justify' text from the cursor
 void LCD50530::noAutoscroll(void) {
-  _displaymode &= ~LCD_CURSORMOVELEFT;
-  command(LCD_ENTRYMODESET | _displaymode);
+  moveDisplayAfterInstruction(LCD_WRITE, LCD_LEFT);
 }
 
 /*********** mid level commands, for sending data/cmds */
 
 inline void LCD50530::command(uint8_t value) {
-  send(value, 0);
+  send(value, 0); 
 }
 
-inline void LCD50530::command(uint8_t value, uint8_t controlpins) {
-  send(value, controlpins);
+void LCD50530::writechar(uint8_t value) {
+  send(value, 1<<_ioc2_pin);  
 }
 
 /************ low level data pushing commands **********/
 
 // write either command or data, with automatic 4/8-bit selection
 void LCD50530::send(uint8_t value, uint8_t controlpins) {
-  while(busyState());	// Wait while the lcd controller is still busy
-  if (_displayfunction & LCD_8BITMODE) {
-	while(busyState());
-    write8bits(value, controlpins); 
-  } else {
-    write4bits(value>>4, controlpins);
-    write4bits(value, controlpins);
-  }
-
-  delay(20);  
+  while(busyState());	// Wait while the lcd controller is still busy       
+  write4bits(value, controlpins);
+  write4bits(value<<4, controlpins);
 }
 
 void LCD50530::pulseExecute(void) {
@@ -213,21 +209,7 @@ void LCD50530::pulseExecute(void) {
 }
 
 void LCD50530::write4bits(uint8_t value, uint8_t controlpins) {
-  PORTD = (value & B11110000);
-
-  if(controlpins) {
-	PORTD |= controlpins;
-  }
-  
-  pulseExecute();
-}
-
-void LCD50530::write8bits(uint8_t value, uint8_t controlpins) {
-  for (int i = 0; i < 8; i++) {
-    pinMode(_data_pins[i], OUTPUT);
-    digitalWrite(_data_pins[i], (value >> i) & 0x01);
-  }
-  
+  PORTD = (value & B11110000) | controlpins;
   pulseExecute();
 }
 
@@ -238,19 +220,19 @@ void LCD50530::write8bits(uint8_t value, uint8_t controlpins) {
 uint8_t LCD50530::busyState() {
   uint8_t state = 0;
   DDRD = B00001111; // Set data pins to input
-  PORTD = B00000000 | 2^_rw_pin;
+  PORTD = 1<<_rw_pin;
   
   digitalWrite(_ex_pin, HIGH);
   delayMicroseconds(2);
-  state = digitalRead(_data_pins[7]);
+  state = digitalRead(_data_pins[3]);
   digitalWrite(_ex_pin, LOW);
   delayMicroseconds(2);
   
-  PORTD = B00000000 | (2^_rw_pin);
+  PORTD = 1<<_rw_pin;
   
   digitalWrite(_ex_pin, HIGH);
   delayMicroseconds(2);
-  state |= digitalRead(_data_pins[7]);
+  state |= digitalRead(_data_pins[3]);
   digitalWrite(_ex_pin, LOW);
   delayMicroseconds(2);
   
--- LCD50530.h
+++ LCD50530.h
@@ -8,22 +8,19 @@ LCD driver for Mitsubishi 50530 compatible displays
 
 #include <inttypes.h>
 
-// commands
-#define LCD_PIN_IOC1 0x00
-#define LCD_PIN_IOC2 0x00
-#define LCD_PIN_RW 0x00
-#define LCD_PIN
-
+// Full commands
 #define LCD_NOOP 0x00
 #define LCD_CLEARDISPLAY 0x01
 #define LCD_RETURNHOME 0x03
+
+// Partial commands (with flags)
 #define LCD_BLINKFREQSET 0x04		// with flags
 #define LCD_WRITERAMULINESET 0x08	// with flags
 #define LCD_UNDERLINEMODESET 0x0C	// with flags
-#define LCD_CURSORSHIFT 0x10		// with flags
+#define LCD_SHIFTCOMMAND 0x10		// with flags
 #define LCD_DISPLAYMODESET 0x20		// with flags
 #define LCD_ENTRYMODESET 0x40		// with flags
-#define LCD_FUNCTIONMODESET 0x90	// with flags
+#define LCD_FUNCTIONMODESET 0xC0	// with flags
 
 //#define LCD_SETCGRAMADDR 0x40
 //#define LCD_SETDDRAMADDR 0x80
@@ -36,18 +33,27 @@ LCD driver for Mitsubishi 50530 compatible displays
 #define LCD_1LINE 0x00
 #define LCD_5x12DOTS 0x00
 #define LCD_5x8DOTS 0x10
-#define LCD_4x40 B1011
+#define LCD_160CHAR 0x03
+#define LCD_192CHAR 0x02
+#define LCD_224CHAR 0x01
+#define LCD_256CHAR 0x00
+
 // TODO: RAM -- setting region (page 28/29 of datasheet)
 
 // flags for display set entry mode
 #define LCD_DISPLAYMOVEREAD 0x01	// Update display address after reading RAM data
 #define LCD_DISPLAYMOVEWRITE 0x02	// Update display address after writing RAM data
 #define LCD_CURSORMOVEREAD 0x08		// Update cursor address after reading RAM data
-#define LCD_CURSORMOVEWRITE 0x16	// Update cursor address after writing RAM data
+#define LCD_CURSORMOVEWRITE 0x10	// Update cursor address after writing RAM data
 #define LCD_DISPLAYMOVERIGHT 0x00	// Display address decremented after instruction
 #define LCD_DISPLAYMOVELEFT 0x04	// Display address incremented after instruction
 #define LCD_CURSORMOVELEFT 0x20		// Cursor address decremented after instruction
 #define LCD_CURSORMOVERIGHT 0x00	// Cursor address incremented after instruction
+
+#define LCD_LEFT	0x04
+#define LCD_RIGHT	0x00
+#define LCD_READ	0x01
+#define LCD_WRITE	0x02
 
 // flags for display on/off control
 #define LCD_DISPLAYON 0x10		// Turn all displays on
@@ -62,25 +68,20 @@ LCD driver for Mitsubishi 50530 compatible displays
 #define LCD_CHRBLINKOFF 0x00	// Character display without blinking
  
 // flags for display/cursor shift command
-#define LCD_DISPLAYMOVE 0x02
-#define LCD_DISPLAYMOVELEFT 0x01
-#define LCD_DISPLAYMOVERIGHT 0x00
-#define LCD_CURSORMOVE 0x08
-#define LCD_CURSORMOVELEFT 0x04
-#define LCD_CURSORMOVERIGHT 0x00
+#define LCD_DISPLAYSHIFT 0x02
+#define LCD_CURSORSHIFT 0x08
+#define LCD_SHIFTLEFT 0x01
+#define LCD_SHIFTRIGHT 0x00
 
 // TODO: flags for underline mode instruction (SU)
 // TODO: flags for Write underline bit instruction (WU)
 // TODO: flags for Set blink frequency instruction (SB)
 
 class LCD50530 {
-public:
-  LCD50530(uint8_t ioc1, uint8_t ioc2, uint8_t rw, uint8_t ex, uint8_t d0, uint8_t d1, uint8_t d2, uint8_t d3, uint8_t d4, uint8_t d5, uint8_t d6, uint8_t d7);
+public:  
   LCD50530(uint8_t ioc1, uint8_t ioc2, uint8_t rw, uint8_t ex, uint8_t d4, uint8_t d5, uint8_t d6, uint8_t d7);
 
-  void init(uint8_t fourbitmode, uint8_t ioc1, uint8_t ioc2, uint8_t rw, uint8_t ex,
-	    uint8_t d0, uint8_t d1, uint8_t d2, uint8_t d3,
-	    uint8_t d4, uint8_t d5, uint8_t d6, uint8_t d7);
+  void init(uint8_t ioc1, uint8_t ioc2, uint8_t rw, uint8_t ex, uint8_t d4, uint8_t d5, uint8_t d6, uint8_t d7);
     
   void begin(uint8_t cols, uint8_t rows, uint8_t charsize = LCD_5x8DOTS);
 
@@ -89,12 +90,22 @@ public:
 
   void noDisplay();
   void display();
-  void noBlink();
-  void blink();
   void noCursor();
   void cursor();
-  void scrollDisplayLeft();
-  void scrollDisplayRight();
+  void noUnderline();
+  void underline();
+  void noCursorBlink();
+  void cursorBlink();
+  void noCharBlink();
+  void charBlink();
+
+  void moveCursorAfterInstruction(uint8_t direction, uint8_t instruction);
+  void moveDisplayAfterInstruction(uint8_t direction, uint8_t instruction);
+  void holdCursor();
+  void holdDisplay();
+
+  void shiftCursor(uint8_t direction);
+  void shiftDisplay(uint8_t direction);
   void leftToRight();
   void rightToLeft();
   void autoscroll();
@@ -103,11 +114,10 @@ public:
   //void createChar(uint8_t, uint8_t[]);
   void setCursor(uint8_t, uint8_t); 
   void command(uint8_t);
-  void command(uint8_t, uint8_t);  
+  void writechar(uint8_t);
 private:
   void send(uint8_t, uint8_t);
   void write4bits(uint8_t, uint8_t);
-  void write8bits(uint8_t, uint8_t);
   void pulseExecute();
   uint8_t busyState();
 
@@ -115,16 +125,16 @@ private:
   uint8_t _ioc2_pin; // LOW: command.  HIGH: character.
   uint8_t _rw_pin; // LOW: write to LCD.  HIGH: read from LCD.
   uint8_t _ex_pin; // activated by a HIGH pulse.
-  uint8_t _data_pins[8];
+  uint8_t _data_pins[4];
 
-  uint8_t _displayfunction;
-  uint8_t _displaycontrol;
+  uint8_t _functionmode;
   uint8_t _displaymode;
+  uint8_t _entrymode;
 
   uint8_t _initialized;
 
-  uint8_t _numlines,_currline;
-
+  uint8_t _numlines, _currline;
+  uint8_t _cols;
 };
 
 #endif
